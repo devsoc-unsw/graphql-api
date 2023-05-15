ab68378db5716fb77f6df7cdd3d2704c1148dbe6
Ray <raiyan.ahmed@gmail.com>
2023-05-16 04:58:38 +1000

    added a cleaner sql file

---
+++ RoomBookings.sql
@@ -0,0 +1,13 @@
+
+--  DROP TYPE BookingTypeEnum cascade;
+ CREATE TYPE BookingTypeEnum AS ENUM ('STD', 'TLB', 'LEC', 'BLOCK', 'SOCIETY', 'SEM', 'WKS', 'OTH', 'LAB', 'EXM', 'LE0', 'MISC', 'INTERNAL', 'LA1', 'TUT', 'LE1', 'LE2');
+
+--  DROP TABLE RoomBookings;
+ CREATE TABLE RoomBookings (
+ 	Id		        SERIAL PRIMARY KEY ,
+ 	BookingType     BookingTypeEnum,
+ 	Name            TEXT NOT NULL,
+ 	RoomId          TEXT NOT NULL,
+ 	Start	        TIMESTAMP,
+ 	Finish	        TIMESTAMP
+ );
--- check.py
+++ check.py
@@ -1,17 +1,4 @@
 
-# DROP TYPE BookingTypeEnum cascade;
-# CREATE TYPE BookingTypeEnum AS ENUM ('STD', 'TLB', 'LEC', 'BLOCK', 'SOCIETY', 'SEM', 'WKS', 'OTH', 'LAB', 'EXM', 'LE0', 'MISC', 'INTERNAL', 'LA1', 'TUT', 'LE1', 'LE2');
-
-# DROP TABLE RoomBookings;
-# CREATE TABLE RoomBookings (
-# 	Id		        SERIAL PRIMARY KEY ,
-# 	BookingType     BookingTypeEnum,
-# 	Name            TEXT NOT NULL,
-# 	RoomId          TEXT NOT NULL,
-# 	Start	        TIMESTAMP,
-# 	Finish	        TIMESTAMP
-# );
-
 
 import json
 
@@ -29,5 +16,3 @@ for id, booking in enumerate(data):
     else:
         sql_query += ', '
 print(sql_query)
-
-
