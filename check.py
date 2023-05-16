d78b56f37dab6abb971bad7a5c5bc6a364e63abc
Ray <raiyan.ahmed@gmail.com>
2023-05-16 12:49:07 +1000

    psychopg2 working

--- check.py
+++ check.py
@@ -1,18 +1,42 @@
 
 
 import json
-
+import psycopg2
+from psycopg2 import Error
 
 f = open('bookings.json')
 data = json.load(f)
-names = set()
-sql_query = "INSERT INTO RoomBookings(BOOKINGTYPE, NAME, ROOMID, START, FINISH) VALUES"
-n = len(data);
-for id, booking in enumerate(data):
-    tp, name, room_id, start, end = booking.values()
-    sql_query += f"(\'{tp}\' , \'{name}\' , \'{room_id}\', \'{start}\', \'{end}\')"
-    if id == n - 1:
-        sql_query += ';'
-    else:
-        sql_query += ', '
-print(sql_query)
+
+def get_query_values():
+    values = []
+    for id, booking in enumerate(data):
+        tp, name, room_id, start, end = booking.values()
+        values.append((tp, name, room_id, start, end))
+    return values
+try:
+    connection = psycopg2.connect(user="postgres",
+                                  password="postgrespassword",
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="postgres")
+
+    cursor = connection.cursor()
+    cmd = "INSERT INTO RoomBookings(BOOKINGTYPE, NAME, ROOMID, START, FINISH) VALUES (%s, %s, %s, %s, %s)"
+    print(get_query_values())
+    cursor.executemany(cmd, get_query_values())
+    connection.commit()
+    # cursor.execute("SELECT * FROM ROOMBOOKINGS;")
+    # row = cursor.fetchone()
+    # while row is not None:
+    #     print(row)
+    #     row = cursor.fetchone()
+except (Exception, Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+finally:
+    if (connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
+
+
+
