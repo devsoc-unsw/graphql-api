e0df67cfa9749655a49323d523b511cf5092f5c8
Franco Reyes <franco.javier.reyes@gmail.com>
2023-05-17 03:33:35 +1000

    Updated to include buildings and rooms

--- RoomBookings.sql
+++ RoomBookings.sql
@@ -1,13 +1,36 @@
 
---  DROP TYPE BookingTypeEnum cascade;
- CREATE TYPE BookingTypeEnum AS ENUM ('CLASS', 'BLOCK', 'SOCIETY', 'MISC', 'INTERNAL');
+--  DROP TABLE Buildings CASCADE;
+ CREATE TABLE Buildings (
+	"id"	TEXT PRIMARY KEY,
+	"name"	TEXT NOT NULL,
+	"lat"	DOUBLE PRECISION NOT NULL,
+	"long"	DOUBLE PRECISION NOT NULL
+ );
+
+--  DROP TABLE RoomBookings CASCADE;
+--  ALTER TABLE Rooms ADD FOREIGN KEY ("buildingId") REFERENCES Buildings("id") ON DELETE CASCADE;
+ CREATE TABLE Rooms (
+	"id"			TEXT PRIMARY KEY,
+	"name"			TEXT NOT NULL,
+	"abbr"			TEXT NOT NULL,	
+	"usage"			TEXT NOT NULL,
+	"capacity"		INTEGER NOT NULL,
+	"school"		TEXT NOT NULL,
+	"buildingId"	TEXT NOT NULL,
+	FOREIGN KEY ("buildingId") REFERENCES Buildings("id") ON DELETE CASCADE
+ );
+
+--  DROP TYPE BookingTypeEnum CASCADE;
+--  CREATE TYPE BookingTypeEnum AS ENUM ('CLASS', 'BLOCK', 'SOCIETY', 'MISC', 'INTERNAL');
 
 --  DROP TABLE RoomBookings;
+--  ALTER TABLE RoomBookings ADD FOREIGN KEY ("roomId") REFERENCES Rooms("id") ON DELETE CASCADE;
  CREATE TABLE RoomBookings (
     "id"            SERIAL PRIMARY KEY,
     "bookingType"   BookingTypeEnum,
     "name"          TEXT NOT NULL,
     "roomId"        TEXT NOT NULL,
-    "start"         TIMESTAMP,
-    "end"           TIMESTAMP
+    "start"         TIMESTAMP NOT NULL,
+    "end"           TIMESTAMP NOT NULL,
+	FOREIGN KEY ("roomId") REFERENCES Rooms("id") ON DELETE CASCADE
  );
