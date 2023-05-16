1c31b0dc8b753fd0d99e35988298db9d36eb3741
Franco Reyes <79299724+francojreyes@users.noreply.github.com>
2023-05-17 01:33:40 +1000

    Update RoomBookings.sql

--- RoomBookings.sql
+++ RoomBookings.sql
@@ -1,13 +1,13 @@
 
 --  DROP TYPE BookingTypeEnum cascade;
- CREATE TYPE BookingTypeEnum AS ENUM ('STD', 'TLB', 'LEC', 'BLOCK', 'SOCIETY', 'SEM', 'WKS', 'OTH', 'LAB', 'EXM', 'LE0', 'MISC', 'INTERNAL', 'LA1', 'TUT', 'LE1', 'LE2');
+ CREATE TYPE BookingTypeEnum AS ENUM ('CLASS', 'BLOCK', 'SOCIETY', 'MISC', 'INTERNAL');
 
 --  DROP TABLE RoomBookings;
  CREATE TABLE RoomBookings (
- 	Id		          SERIAL PRIMARY KEY ,
- 	BookingType     BookingTypeEnum,
- 	Name            TEXT NOT NULL,
- 	RoomId          TEXT NOT NULL,
- 	Start	          TIMESTAMP,
- 	Finish	        TIMESTAMP
+ 	'id'		            SERIAL PRIMARY KEY ,
+ 	'bookingType'     BookingTypeEnum,
+  'name'            TEXT NOT NULL,
+ 	'roomId'          TEXT NOT NULL,
+ 	'start'	          TIMESTAMP,
+ 	'end'	            TIMESTAMP
  );
