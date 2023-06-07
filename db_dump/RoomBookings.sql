127580d1fbe16515fee65ca8ff819b0b7f6cdeec
Raiyan Ahmed <80839724+Rayahhhmed@users.noreply.github.com>
2023-06-07 22:20:22 +1000

    Update RoomBookings.sql

--- db_dump/RoomBookings.sql
+++ db_dump/RoomBookings.sql
@@ -30,7 +30,7 @@
     "bookingType"   BookingTypeEnum,
     "name"          TEXT NOT NULL,
     "roomId"        TEXT NOT NULL,
-    "start"         TIMESTAMP NOT NULL,
-    "end"           TIMESTAMP NOT NULL,
-	FOREIGN KEY ("roomId") REFERENCES Rooms("id") ON DELETE CASCADE
+    "start"         TIMESTAMPTZ NOT NULL,
+    "end"           TIMESTAMPTZ NOT NULL,
+     FOREIGN KEY ("roomId") REFERENCES Rooms("id") ON DELETE CASCADE
  );
